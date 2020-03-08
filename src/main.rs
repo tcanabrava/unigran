00a18f4a6864e2d315cccdd93babd2275ca89dd6
Tomaz Canabrava <tcanabrava@kde.org>
2020-03-08 19:08:11 +0100

    Add parsed arguments to the imc calculator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate clap;
+
 fn main() {
-    println!("Hello, world!");
+    let args = clap::App::new("IMC Calculations App")
+        .version("1.0")
+        .author("Tomaz Canabrava")
+        .about("Calculates the IMC for a single person")
+        .arg(clap::Arg::with_name("height")
+            .short("h")
+            .long("height")
+            .help("Your height in meters")
+            .takes_value(true)
+            .required(true)
+        )
+        .arg(clap::Arg::with_name("weight")
+            .short("w")
+            .long("weight")
+            .help("Your weight in kgs")
+            .takes_value(true)
+            .required(true)
+        ).get_matches();
+    println!("{:#?}", args);
 }
